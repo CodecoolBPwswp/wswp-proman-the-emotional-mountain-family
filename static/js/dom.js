d7ed26e97026003436ee3a68dc2340c31372fa7c
lterray <laszlo.terray@gmail.com>
2018-05-02 16:57:32 +0200

    Add function which can extend existing DOM elements with string without overwriting event listeners

--- static/js/dom.js
+++ static/js/dom.js
@@ -13,6 +13,21 @@ let dom = {
     showCards: function(cards) {
         // shows the cards of a board
         // it adds necessary event listeners also
+    },
+    appendToElement: function(elementToExtend, textToAppend, prepend = false) {
+        // function to append new DOM elements (represented by a string) to an existing DOM element
+        let fakeDiv = document.createElement('div');
+        fakeDiv.innerHTML = textToAppend.trim();
+
+        for (childNode of fakeDiv.childNodes) {
+            if (prepend) {
+                elementToExtend.prependChild(childNode);
+            } else {
+                elementToExtend.appendChild(childNode);
+            }
+        }
+
+        return elementToExtend.lastChild;
     }
     // here comes more features
 }
